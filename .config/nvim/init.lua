e9a1dc1d31f480a8b928761f163c6f9bb8faadbe
banshay <importantbopp@gmail.com>
2025-07-12 16:41:02 +0200

    test

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -239,7 +239,7 @@ require("lazy").setup({
 								callSnippet = "Replace",
 							},
 							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-							-- diagnostics = { disable = { 'missing-fields' } },
+							diagnostics = { disable = { "missing-fields" } },
 						},
 					},
 				},
@@ -269,7 +269,9 @@ require("lazy").setup({
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
 			})
-			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+			require("mason-tool-installer").setup({
+				ensure_installed = ensure_installed,
+			})
 
 			require("mason-lspconfig").setup({
 				handlers = {
