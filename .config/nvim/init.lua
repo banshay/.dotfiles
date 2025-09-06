3c663ab706b8ddeb67ca400129e02a6d18de1ce6
banshay <importantbopp@gmail.com>
2025-09-06 23:25:48 +0200

    pc

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -258,6 +258,11 @@ require("lazy").setup({
 						},
 					},
 				},
+				ocamllsp = {
+					cmd = {
+						"./_opam/bin/ocamllsp",
+					},
+				},
 				zls = {
 					settings = {
 						inlay_hints_show_builtin = false,
@@ -886,6 +891,63 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = highlight_group,
 	pattern = "*",
 })
+
+-- [[ Configure Telescope ]]
+-- See `:help telescope` and `:help telescope.setup()`
+-- require('telescope').setup {
+--   defaults = {
+--     path_display = {
+--       truncate = {},
+--     },
+--     mappings = {
+--       i = {
+--         ['<C-u>'] = false,
+--         ['<C-d>'] = false,
+--       },
+--     },
+--   },
+--   pickers = {
+--     lsp_references = {
+--       show_line = false,
+--     },
+--   },
+--   extensions = {
+--     file_browser = {
+--       hijack_netrw = true,
+--     }
+--   }
+-- }
+
+-- require("telescope.builtin").lsp_references({
+--   show_line = false,
+-- })
+
+-- Enable telescope fzf native, if installed
+-- pcall(require('telescope').load_extension, 'fzf')
+-- Enable telescope file browser extension
+-- pcall(require('telescope').load_extension, 'file_browser')
+
+-- See `:help telescope.builtin`
+-- vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+-- vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+-- vim.keymap.set('n', '<leader>/', function()
+--   -- You can pass additional configuration to telescope to change theme, layout, etc.
+--   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+--     winblend = 10,
+--     previewer = false,
+--   })
+-- end, { desc = '[/] Fuzzily search in current buffer' })
+--
+-- vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+-- vim.keymap.set('n', '<leader>gc', require('telescope.builtin').git_commits, { desc = 'Search [G]it [C]ommits' })
+-- vim.keymap.set('n', '<leader>gh', require('telescope.builtin').git_stash, { desc = 'Search [G]it Stas[h]' })
+vim.keymap.set("n", "<leader>gb", require("telescope.builtin").git_branches, { desc = "Search [G]it [B]ranches" })
+-- vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+-- vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+-- vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+-- vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+-- vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+-- vim.keymap.set('n', '<leader>t', require('telescope.builtin').treesitter, { desc = 'Treesitter' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
