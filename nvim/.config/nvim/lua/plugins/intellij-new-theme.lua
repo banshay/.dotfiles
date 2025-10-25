a6cc3e70c84eb7487461fa13380514113329d94e
banshay <importantbopp@gmail.com>
2025-10-25 20:00:13 +0200

    --amend

---
+++ nvim/.config/nvim/lua/plugins/intellij-new-theme.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "banshay/intellij-new-theme-nvim",
+    -- name = "intellij-new-theme-nvim",
+    -- dir = "~/private/projects/intellij-new-theme-nvim",
+    -- dev = true,
+    config = function()
+      vim.cmd("colorscheme intellij-new")
+    end,
+  }
+}
---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+
+  { "nvim-treesitter/playground" },
+
+  {
+    -- Highlight, edit, and navigate code
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    build = ":TSUpdate",
+  },
+}
