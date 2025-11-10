ce9a4f2a1f1279d753cd4b4e21922c133f368067
bopplu <lukas@likemagic.tech>
2025-11-11 00:17:44 +0100

    Add missing files

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,19 @@
+return {
+	"neovim/nvim-lspconfig",
+	opts = {
+		servers = {
+			zls = {
+				settings = {
+					zls = {
+						inlay_hints_show_builtin = false,
+						inlay_hints_show_parameter_name = true,
+						inlay_hints_hide_redundant_param_names = true,
+						inlay_hints_hide_redundant_param_names_last_token = true,
+						inlay_hints_show_variable_type_hints = false,
+						inlay_hints_show_struct_literal_field_type = false,
+					},
+				},
+			},
+		},
+	},
+}
---
+++ nvim/.config/nvim/lua/plugins/snacks-overrides.lua
@@ -0,0 +1,17 @@
+return {
+	"folke/snacks.nvim",
+	opts = {
+		scroll = {
+			enabled = false, -- Disable scrolling animations
+		},
+		notifier = {
+			timeout = 10000,
+		},
+		dashboard = {
+			enabled = false,
+		},
+		words = {
+			enabled = false,
+		},
+	},
+}
