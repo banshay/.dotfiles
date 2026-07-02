1f68d0d464767d0539422d0abd74128936fe50b8
bopplu <lukas@likemagic.tech>
2026-07-02 16:55:23 +0200

    fix opening files from lazygit

--- nvim/.config/nvim/lua/plugins/snacks-overrides.lua
+++ nvim/.config/nvim/lua/plugins/snacks-overrides.lua
@@ -1,3 +1,17 @@
+_G.LazygitEdit = function(path)
+	for _, w in ipairs(vim.api.nvim_list_wins()) do
+		if vim.bo[vim.api.nvim_win_get_buf(w)].filetype == "snacks_terminal" then
+			pcall(vim.api.nvim_win_close, w, true)
+		end
+	end
+	vim.cmd.edit(vim.fn.fnameescape(path))
+end
+
+_G.LazygitEditLine = function(path, line)
+	LazygitEdit(path)
+	pcall(vim.api.nvim_win_set_cursor, 0, { tonumber(line) or 1, 0 })
+end
+
 return {
 	"folke/snacks.nvim",
 	opts = {
@@ -19,7 +33,11 @@ return {
 		lazygit = {
 			configure = true,
 			config = {
-				os = { editPreset = "nvim-remote" },
+				os = {
+					editPreset = "nvim-remote",
+					edit = 'nvim --server "$NVIM" --remote-expr "v:lua.LazygitEdit(\'{{filename}}\')"',
+					editAtLine = 'nvim --server "$NVIM" --remote-expr "v:lua.LazygitEditLine(\'{{filename}}\', {{line}})"',
+				},
 			},
 		},
 	},
