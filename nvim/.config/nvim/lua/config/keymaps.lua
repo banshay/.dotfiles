a0289dfd5238d24f74da50cb87887da90f4f500d
bopplu <lukas@likemagic.tech>
2026-08-26 15:39:08 +0200

    More keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -205,3 +205,8 @@ vim.keymap.set("n", "<leader>cf", function()
 		apply = true,
 	})
 end)
+
+vim.keymap.set("n", "<leader>ca", function()
+	LazyVim.lsp.action.source()
+end, { desc = "Source Action", has = "codeAction" })
+vim.keymap.set("n", "<leader>cA", vim.lsp.buf.code_action, { desc = "Code Action", has = "codeAction" })
